48f06e8c5664b3914e20b57e83d0a575a8a4561f
Moisuk-Dmitri <moisuk.d@gmail.com>
2024-09-24 14:20:35 +0300

    Initial commit

---
+++ skypro-libraries/src/main/java/com/skypro_libraries/skypro_libraries/SkyproLibrariesApplication.java
@@ -0,0 +1,12 @@
+package com.skypro_libraries.skypro_libraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproLibrariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproLibrariesApplication.class, args);
+	}
+}
